3b7afacb3ab3c83957c8114968f22d4889dcdc49
Anna <kmthtk937@gmail.com>
2025-10-16 22:11:34 +0900

    勉強

--- frontend/src/app/practice/page.tsx
+++ frontend/src/app/practice/page.tsx
@@ -1,10 +1,26 @@
-'use client'; //このコードはブラウザ側で動かすよ
- import React from 'react';//Reactを使うよ
+'use client';
 
- export default function PracticePage () {
-    return (
-        <div>
-            <h1>カウンター</h1>
-        </div>
-    );
- }
+// ① useEffect をReactからさらに追加で読み込む
+import React, { useState, useEffect } from 'react';
+
+export default function PracticePage() {
+  const [count, setCount] = useState(0);
+
+  // ② useEffectを追加
+  //    この中の処理は、画面が描画（or 再描画）された「後」に毎回実行される
+  useEffect(() => {
+    console.log(`現在のカウントは ${count} です。`);
+  });
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>カウンター</h1>
+      <p>現在のカウント: {count}</p>
+      <button onClick={handleIncrement}>+1する</button>
+    </div>
+  );
+}
